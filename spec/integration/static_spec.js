9e843c6c0a10d467537f8632132402407eb29c8e
Richard Chan <rchan817@yahoo.com>
2020-06-04 13:31:47 -0700

    added routes

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+  describe("GET /", () => {
+
+    it("should return status code 200", (done) => {
+      axios.get(base)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        done();
+      })
+      .catch((err) => {
+        console.log(error);
+        done();
+      })
+    });
+  });
+
+});
